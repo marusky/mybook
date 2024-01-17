cd43fcae322da94c5ec88c9a21ffba1b51dbb841
marusky <adrianmarusakza@gmail.com>
2024-01-17 12:19:12 +0100

    Add Item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_09_125620) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_17_111509) do
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.float "expected_cost"
+    t.boolean "is_done"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "body"
