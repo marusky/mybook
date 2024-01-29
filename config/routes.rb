7a5cedaadbee70d153e1f305d72c92ced3305abe
marusky <adrianmarusakza@gmail.com>
2024-01-29 11:03:37 +0100

    Add edit and update to items

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,10 @@ Rails.application.routes.draw do
 
   get 'items', to: 'items#index'
   post 'items', to: 'items#create'
-  get 'items/new', to: 'items#new'
+  get 'items/new', to: 'items#new', as: :new_item
   get 'items/:id', to: 'items#show', as: :item
+  get 'items/:id/edit', to: 'items#edit', as: :edit_item
+  patch 'items/:id', to: 'items#update'
 
   root to: 'pages#home'
 end
