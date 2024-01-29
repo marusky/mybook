81018d9f62e452b719cd4c6ebf5e923d50dcfda7
marusky <adrianmarusakza@gmail.com>
2024-01-29 10:21:14 +0100

    Add edit and update for notes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'notes', to: 'notes#index'
   post 'notes', to: 'notes#create'
-  get 'notes/new', to: 'notes#new'
+  get 'notes/new', to: 'notes#new', as: :new_note
   get 'notes/:id', to: 'notes#show', as: :note
+  patch 'notes/:id', to: 'notes#update'
+  get 'notes/:id/edit', to: 'notes#edit', as: :edit_note
 
   get 'items', to: 'items#index'
   post 'items', to: 'items#create'
