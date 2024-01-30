6d0d511ba724df98bc069173cdc2d78d9bfa430b
marusky <adrianmarusakza@gmail.com>
2024-01-30 10:45:10 +0100

    Clean up items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,38 +1,45 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
     @items = Item.all
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def new
+    @item = Item.new
   end
 
   def create
-    @item = Item.create(title: params[:title], expected_cost: params[:expected_cost], is_done: params[:is_done])
+    @item = Item.create(item_params)
 
     redirect_to @item
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
-
-    @item.update(title: params[:title], expected_cost: params[:expected_cost], is_done: params[:is_done])
+    @item.update(item_params)
 
     redirect_to @item
   end
 
   def destroy
-    @item = Item.find(params[:id])
-    
     @item.delete
   
     redirect_to @item
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :expected_cost, :is_done)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  def sanitize_for_mass_assignment(attributes)
+    attributes
+  end
+
+  alias :sanitize_forbidden_attributes :sanitize_for_mass_assignment
 end
